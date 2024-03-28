73385f4880caf03343239238203f46b1e530397f
TheChunkyPumpkinhead <chriscampos@remainobscure.com>
2024-03-28 11:58:15 -0700

    created a reuseable component H1

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,15 @@
+import styled from "styled-components";
+// reuseable component
+const H1 = styled.h1`
+  font-size: 30px;
+  font-weight: 600;
+`;
+
 function App() {
-  return <div>Hello World</div>;
+  return (
+    <div>
+      <H1>The Obscure House</H1>
+    </div>
+  );
 }
 export default App;
