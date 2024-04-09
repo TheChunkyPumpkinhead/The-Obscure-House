63f585fd1efdfd0f963ddea57191627c038040e6
TheChunkyPumpkinhead <chriscampos@remainobscure.com>
2024-04-08 19:54:01 -0700

    styled the new components

--- src/ui/SideBar.jsx
+++ src/ui/SideBar.jsx
@@ -1,5 +1,13 @@
+import styled from "styled-components";
+
+const StyledSidebar = styled.aside`
+  background-color: purple;
+  padding: 1.2rem 4.8rem;
+  border-bottom: 1px solid var(--color-grey-100);
+`;
+
 function SideBar() {
-  return <aside>SideBar</aside>;
+  return <StyledSidebar>SideBar</StyledSidebar>;
 }
 
 export default SideBar;
