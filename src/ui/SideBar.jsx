f86ea380c1bf0a549973f0e8d3fae994077cf619
TheChunkyPumpkinhead <chriscampos@remainobscure.com>
2024-04-08 20:05:28 -0700

    styled components

--- src/ui/SideBar.jsx
+++ src/ui/SideBar.jsx
@@ -1,9 +1,11 @@
 import styled from "styled-components";
 
 const StyledSidebar = styled.aside`
-  background-color: purple;
+  background-color: var(--color-grey-0);
   padding: 1.2rem 4.8rem;
   border-bottom: 1px solid var(--color-grey-100);
+
+  grid-row: 1 / -1;
 `;
 
 function SideBar() {
