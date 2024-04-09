a2e7cba80dbd084a53f098a74e79f16de6cf64bd
TheChunkyPumpkinhead <chriscampos@remainobscure.com>
2024-04-08 19:37:20 -0700

    Updated 2 components

--- src/ui/AppLayout.jsx
+++ src/ui/AppLayout.jsx
@@ -1,10 +1,15 @@
 import { Outlet } from "react-router-dom";
+import SideBar from "./SideBar";
+import Header from "./Header";
 
 function AppLayout() {
   return (
     <div>
-      <p>App Layout</p>
-      <Outlet />
+      <Header />
+      <SideBar />
+      <main>
+        <Outlet />
+      </main>
     </div>
   );
 }
