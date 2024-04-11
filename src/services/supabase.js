62c8d3bdc65287742b117498d9dd4c7d76839b4c
TheChunkyPumpkinhead <chriscampos@remainobscure.com>
2024-04-11 15:31:35 -0700

    added the supabaseKey

--- src/services/supabase.js
+++ src/services/supabase.js
@@ -1,5 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 const supabaseUrl = "https://phplhwuyexoqughwimtm.supabase.co";
-const supabaseKey = process.env.SUPABASE_KEY;
+const supabaseKey =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBocGxod3V5ZXhvcXVnaHdpbXRtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTI3NzU5ODksImV4cCI6MjAyODM1MTk4OX0.68bS-hjvHniqsK1FQi9gJzK2nFU5xMFqPutIjS4Jy60";
 const supabase = createClient(supabaseUrl, supabaseKey);
 export default supabase;
