721344ddef619b1d86901f3d6d0a8c5e8ae2975a
TheChunkyPumpkinhead <chriscampos@remainobscure.com>
2024-04-12 10:37:06 -0700

    added spinner styling

--- src/features/cabins/CabinTable.jsx
+++ src/features/cabins/CabinTable.jsx
@@ -1,4 +1,7 @@
+import { useQuery } from "@tanstack/react-query";
 import styled from "styled-components";
+import { getCabins } from "../../services/apiCabins";
+import Spinner from "../../ui/Spinner";
 
 const Table = styled.div`
   border: 1px solid var(--color-grey-200);
@@ -25,7 +28,16 @@ const TableHeader = styled.header`
 `;
 
 function CabinTable() {
-  return <div>Table</div>;
+  const {
+    isLoading,
+    data: cabins,
+    error,
+  } = useQuery({
+    queryKey: ["cabin"],
+    queryFn: getCabins,
+  });
+  if (isLoading) return <Spinner />;
+  <div>Table</div>;
 }
 
 export default CabinTable;
