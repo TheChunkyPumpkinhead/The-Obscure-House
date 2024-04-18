225ecab109ee6b0edca05e3441f6b2b4ec4631a6
TheChunkyPumpkinhead <chriscampos@remainobscure.com>
2024-04-17 17:01:22 -0700

    created a button to load the form

--- src/pages/Cabins.jsx
+++ src/pages/Cabins.jsx
@@ -1,8 +1,13 @@
+import { useState } from "react";
 import CabinTable from "../features/cabins/CabinTable";
 import Heading from "../ui/Heading";
 import Row from "../ui/Row";
+import Button from "../ui/Button";
+import CreateCabinForm from "../features/cabins/CreateCabinForm";
 
 function Cabins() {
+  const [showForm, setShowForm] = useState(false);
+
   return (
     <>
       <Row type="horizontal">
@@ -12,6 +17,11 @@ function Cabins() {
       <Row>
         <CabinTable />
       </Row>
+      {/* Problem- this button is not in the place the course says it should be. It is to be in the cabin row */}
+      <Button onClick={() => setShowForm((show) => !show)}>
+        Add new Cabin
+      </Button>
+      {showForm && <CreateCabinForm />}
     </>
   );
 }
