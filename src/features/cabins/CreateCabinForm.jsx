d2b3584e8d091b98adef256730b07d3e285b4e4c
TheChunkyPumpkinhead <chriscampos@remainobscure.com>
2024-06-13 22:18:14 -0700

    set atributes properties

--- src/features/cabins/CreateCabinForm.jsx
+++ src/features/cabins/CreateCabinForm.jsx
@@ -151,7 +151,13 @@ function CreateCabinForm() {
       </FormRow>
 
       <FormRow label="Cabin photo">
-        <FileInput id="image" accept="image/*" />
+        <FileInput
+          id="image"
+          accept="image/*"
+          {...register("image", {
+            required: "This field is required",
+          })}
+        />
       </FormRow>
 
       <FormRow>
